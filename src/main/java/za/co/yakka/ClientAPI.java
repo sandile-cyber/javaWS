a8ea24c13f7db343b74ab2ea1b245ee8337e035b
sandile-cyber <sandile@bbd.co.za>
2019-06-24 09:40:55 +0200

    changed how the getCurrencyCodes method is written

--- src/main/java/za/co/yakka/ClientAPI.java
+++ src/main/java/za/co/yakka/ClientAPI.java
@@ -26,101 +26,103 @@ import jpa.Client;
 @Path("/client")
 public class ClientAPI {
 
-		@Inject
-		ClientEJB clientEJB;
-		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("getClient/{id}")
-		public Client getClient(@PathParam("id") int id) {
-			return clientEJB.getClient(id);
-		}
-		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("helloWorld")
-		public String helloWorld() {
-			return "Hello world \n";
-		}
-		
-		@GET
-		@Produces(MediaType.APPLICATION_JSON)
-		@Path("/getClientList")
-		public List<Client>  getClientList(){
-		
-			return clientEJB.getAll();
-		
-		}
-		
-		@POST
-		@Path("/addClient")
-		public void addClient(@QueryParam("id") String id, @QueryParam("name") String name) {
-			clientEJB.addClient(Integer.parseInt(id), name);
-		}
-		
-		@PUT
-		@Path("/updateClient")
-		public void updateClient(@QueryParam("id") String id, @QueryParam("name") String name) {
-			System.out.println("============= "+id);
-			clientEJB.updateClient(Integer.parseInt(id), name);
-		}
-		
-		@GET
-		@Path("/getCurrencyCodes")
-		@Produces(MediaType.APPLICATION_JSON)
-		public List<String> getCurrencyCodes(){
-			
-			String results;
-			StringBuffer response = null;
-			
-			try {
+	@Inject
+	ClientEJB clientEJB;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("getClient/{id}")
+	public Client getClient(@PathParam("id") int id) {
+		return clientEJB.getClient(id);
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("helloWorld")
+	public String helloWorld() {
+		return "Hello world \n";
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/getClientList")
+	public List<Client> getClientList() {
+
+		return clientEJB.getAll();
+
+	}
+
+	@POST
+	@Path("/addClient")
+	public void addClient(@QueryParam("id") String id, @QueryParam("name") String name) {
+		clientEJB.addClient(Integer.parseInt(id), name);
+	}
+
+	@PUT
+	@Path("/updateClient")
+	public void updateClient(@QueryParam("id") String id, @QueryParam("name") String name) {
+		clientEJB.updateClient(Integer.parseInt(id), name);
+	}
+
+	@GET
+	@Path("/getCurrencyCodes")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<String> getCurrencyCodes() {
+
+		String results;
+		StringBuffer response = null;
+
+		try {
+
 			URL url = new URL("http://apilayer.net/api/list?access_key=55475920264d75362b9d74b7a2ea3c3b");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			int responseCode = connection.getResponseCode();
-			
-			if ((responseCode >= 200) && (responseCode < 300)){
-				
-				BufferedReader inputBufferedReader = new BufferedReader( new InputStreamReader(connection.getInputStream()));
-				
-				 response = new StringBuffer();
-				
-				while((results = inputBufferedReader.readLine()) != null) {
+
+			if ((responseCode >= 200) && (responseCode < 300)) {
+
+				BufferedReader inputBufferedReader = new BufferedReader(
+						new InputStreamReader(connection.getInputStream()));
+
+				response = new StringBuffer();
+
+				while ((results = inputBufferedReader.readLine()) != null) {
 					response.append(results);
 				}
-				
+
 				inputBufferedReader.close();
 
-				}
-			
 			}
-			catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			if (response != null) {
-			
-				JSONObject responseJSONObject = new JSONObject(response.toString());
-				JSONObject currencyCodes = (JSONObject) responseJSONObject.get("currencies");
-				System.out.println("**************************");
-				System.out.println("--- " + currencyCodes.keySet().toString());
-				
-				Iterator keyIterator = currencyCodes.keys();
-				List<String> keyList = new ArrayList<String>();
-				
-				while(keyIterator.hasNext()) {
-					String key = (String) keyIterator.next();
-					keyList.add(key);
-					
-				}
-				System.out.println("Right Here");
-				
-				return keyList;
-				
-			}
-	
-			return null;
-			
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
+
+		if (response != null) {
+
+			return parseJSONObject(response);
+
+		}
+
+		return null;
+
+	}
+
+	private List<String> parseJSONObject(StringBuffer apiResponse) {
+
+		JSONObject responseJSONObject = new JSONObject(apiResponse.toString());
+		JSONObject currencyCodes = (JSONObject) responseJSONObject.get("currencies");
+
+		Iterator keyIterator = currencyCodes.keys();
+		List<String> keyList = new ArrayList<String>();
+
+		while (keyIterator.hasNext()) {
+			String key = (String) keyIterator.next();
+			keyList.add(key);
+		}
+
+		return keyList;
+
+	}
+
 }
